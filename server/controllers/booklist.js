8a3bacb1b688e53f98a3ba1a8adb10b8c85c3717
Shiny_jun <34482015+shiny-jun@users.noreply.github.com>
2018-12-14 18:28:29 +0800

    Update booklist.js

--- server/controllers/booklist.js
+++ server/controllers/booklist.js
@@ -1,8 +1,20 @@
-const { mysql } = require('../qcloud')
+const {
+  mysql
+} = require('../qcloud')
 module.exports = async (cxt) => {
-    const books = await mysql('books').select('*')
-    // .orderBy('id','desc')
-    cxt.state.data = {
-        list: books
-    }
+  const books = await mysql('books')
+    .select('books.*', 'csessioninfo')
+    .join('csessioninfo', 'books.openid', 'csessioninfo.open_id')
+    .orderBy('books.id', 'desc')
+  // .orderBy('id','desc')
+  cxt.state.data = {
+    list: books.map(v => {
+      const info = JSON.parse(v.user_info)
+      return Object.assign({},v,{
+          user_info:{
+              nickName:info.nickName
+          }
+      })
+    })
+  }
 }
